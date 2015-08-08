6b88cecbb6670fad38da801fb1d5afa98b966559
David Rodrigues <David@macbook-pro-de-david-1.home>
2015-08-08 17:21:57 +0200

    Add template project

---
+++ iOS-Kodi-Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Kodi-Remote
+//
+//  Created by David Rodrigues on 08/08/2015.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
